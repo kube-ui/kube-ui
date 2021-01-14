c0e4402fe95840a660f2445502162c44729d1368
cmacs <colin.macsweeny2@sky.uk>
2021-01-14 14:37:39 +0000

    Added getNamespaceDetails and changed other functions to use promises

--- utils/kubectl.js
+++ utils/kubectl.js
@@ -12,14 +12,14 @@ const login = () => {
                 return;
             }
             
-            if (stderr) {
-                console.error(`stderr: ${stderr}`);
-                reject({
-                    success: false,
-                    error: stderr
-                })
-                return;
-            }
+            // if (stderr) {
+            //     console.error(`stderr: ${stderr}`);
+            //     reject({
+            //         success: false,
+            //         error: stderr
+            //     })
+            //     return;
+            // }
             
             console.log(`stdout: ${stdout}`);
             resolve({
@@ -30,66 +30,135 @@ const login = () => {
 }
 
 const getNamespaces = () => {
-    exec("kubectl --context=dev get namespaces -o json", {maxBuffer: 1024 * 5000}, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-
-        const result = JSON.parse(stdout);
-
-
-        const data = result.items.map(item => {
-            return {
-                "name": item.metadata.name,
-                "team": item.metadata.labels.team,
-                "area": item.metadata.labels['area-name'],
-                "slack": item.metadata.annotations.slack    
+    return new Promise(function(resolve, reject){
+        exec("kubectl --context=dev get namespaces -o json", {maxBuffer: 1024 * 5000}, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                reject({
+                    success: false,
+                    error: error.message
+                })
             }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                reject({
+                    success: false,
+                    error: error.message
+                })
+            }
+
+            const result = JSON.parse(stdout);
+
+
+            const data = result.items.map(item => {
+                return {
+                    "name": item.metadata.name,
+                    "team": item.metadata.labels.team,
+                    "area": item.metadata.labels['area-name'],
+                    "slack": item.metadata.annotations.slack    
+                }
+            });
+
+
+            const stringifiedJson = JSON.stringify(data)
+            // console.log(`stdout: ${stdout}`);
+            console.log(`${stringifiedJson}`);
+            resolve({
+                success: true,
+                data: data
+            });
         });
+    });
+}
 
+const getNamespaceDetails = (namespace) => {
+    return new Promise(function(resolve, reject){
+        exec(`kubectl --context=dev --namespace=${namespace} get deployments -o json`, {maxBuffer: 1024 * 5000}, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error reading deployments from kubernetes: ${error.message}`);
+                reject({
+                    success: false,
+                    error: error.message
+                })
+                return;
+            }
 
-        // const stringifiedJson = JSON.stringify(data)
-        // console.log(`stdout: ${stdout}`);
-        // console.log(`${stringifiedJson}`);
-        return data;
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+                reject({
+                    success: false,
+                    error: stderr
+                })
+                return;
+            }
+
+            const result = JSON.parse(stdout);
+
+            const data = result.items.map(item => {
+                return {
+                    "service": item.metadata.labels['app.kubernetes.io/name'],
+                    "pods": item.status.replicas,
+                    "ready-pods": item.status.readyReplicas,
+                    "lastDeployed": item.status.conditions.find(
+                        element => element.reason === "NewReplicaSetAvailable"
+                        ).lastUpdateTime  
+                }
+            });
+            const stringifiedJson = JSON.stringify(data)
+            // console.log(`stdout: ${stdout}`);
+            console.log(`${stringifiedJson}`);
+            resolve({
+                success: true,
+                data: data
+            });
+        });
     });
 }
 
 const getPods = (namespace) => {
-    exec(`kubectl --context=dev --namespace=${namespace} get po -o json`, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-
-        const result = JSON.parse(stdout);
-
-        const data = result.items.map(item => {
-            return {
-                "name": item.metadata.labels['app.kubernetes.io/name'],
-                "status": item.status.phase,
-                "startTime": item.status.startTime  
+    return new Promise(function(resolve, reject){
+        exec(`kubectl --context=dev --namespace=${namespace} get po -o json`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error reading deployments from kubernetes: ${error.message}`);
+                reject({
+                    success: false,
+                    error: error.message
+                })
+                return;
             }
-        });
-        
-        // const stringifiedJson = JSON.stringify(data)
-        // console.log(stringifiedJson)
-        return data;
 
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+                reject({
+                    success: false,
+                    error: stderr
+                })
+                return;
+            }
+
+            const result = JSON.parse(stdout);
+
+            const data = result.items.map(item => {
+                return {
+                    "service": item.metadata.labels['app.kubernetes.io/name'],
+                    "status": item.status.phase,
+                    "startTime": item.status.startTime  
+                }
+            });
+            
+            const stringifiedJson = JSON.stringify(data)
+            console.log(stringifiedJson)
+            resolve({
+                success: true,
+                data: data
+            });
+        });
     });
 }
 
 module.exports = {
     login,
     getNamespaces,
+    getNamespaceDetails,
     getPods
 }
