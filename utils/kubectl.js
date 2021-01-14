d6bde19f1ad6f3b70aefd12a921b496de9afeaff
Samir Benzenine <samir.benzenine@gmail.com>
2021-01-14 10:07:05 +0000

    Add kubectl client

--- utils/kubectl.js
+++ utils/kubectl.js
@@ -1,7 +1,11 @@
 const { exec } = require("child_process");
 
+const login = () => {
+    
+}
+
 const getPods = () => {
-    exec("kubectl --context=dev --namespace=is-e2e get po -o json", (error, stdout, stderr) => {
+    exec("kubectl --context=dev get namespaces -o json", (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             return;
@@ -11,6 +15,25 @@ const getPods = () => {
             return;
         }
         console.log(`stdout: ${stdout}`);
+    });
+}
+
+const getPods = (context= "dev", namepsace) => {
+    exec(`kubectl --context=dev --namespace=${namepsace} get po -o json`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+
+        const result = JSON.parse(stdout)
+        const data = {
+            status: result.status
+        }
     });
 }
 
---
+++ utils/kubectl.md
@@ -0,0 +1,40 @@
+# Kubectl client
+
+## Login
+
+```
+{
+
+}
+```
+
+## Get Namespaces
+
+```
+[
+    {
+        teams: String,
+        area: String,
+        slack: String
+    }
+]
+```
+
+## Get pods
+
+```
+{
+    service: String,
+    status: String,
+    pods: Number,
+    ready-pods: Number,
+}
+```
+
+## Get deployemnts
+
+```
+{
+    
+}
+```
